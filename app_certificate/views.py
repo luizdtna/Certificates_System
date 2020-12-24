a066819f5b9835ffd6ba8b3f046f47b1ade0301d
Luiz Araujo <Luiz.araujodtna@gmail.com>
2020-12-24 15:12:07 -0300

    Beta versao1

--- app_certificate/views.py
+++ app_certificate/views.py
@@ -7,7 +7,7 @@ from django.shortcuts import render, resolve_url, redirect
 from django.urls import reverse_lazy
 from django.views import View
 from django.views.generic import FormView, UpdateView
-
+from django.contrib import messages
 from projeto import settings
 from .models import Certificate, CustomUser
 from .forms import NewCertificatesForm, CustomAuthenticationForm
@@ -15,9 +15,9 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+
+
 # Create your views here.
-
-
 
 
 class My_Login(LoginView):
@@ -35,19 +35,19 @@ class GestaoHome(LoginRequiredMixin, View):
 
     def get(self, request):
         if request.user.is_staff:
-            return render(request,'app_certificate/gestao.html')
+            return render(request, 'app_certificate/gestao.html')
         else:
             return redirect('certificado_url')
 
-class FileFieldView(LoginRequiredMixin,FormView):
 
+class FileFieldView(LoginRequiredMixin, FormView):
     form_class = NewCertificatesForm
     template_name = 'app_certificate/upload.html'  # Replace with your template.
     success_url = reverse_lazy('uploadcertificados_url')  # Replace with your URL or reverse().
 
     def form_valid(self, form):
         """If the form is valid, redirect to the supplied URL."""
-        return self.render_to_response({'form':form, 'success':True})
+        return self.render_to_response({'form': form, 'success': True})
 
     def list_to_vector(self, separator):
         dados = []
@@ -55,28 +55,28 @@ class FileFieldView(LoginRequiredMixin,FormView):
         emails = []
         f = open("statics/people_list.txt", "r")
         for line in f:
-            #pega as linhas do arquivo, e remove o \n
+            # pega as linhas do arquivo, e remove o \n
             line = line.rstrip('\n')
-            #divide a string em duas tabelas, uma com o nome e outra com o email
+            # divide a string em duas tabelas, uma com o nome e outra com o email
             if separator == 'ponto_virgula':
                 line = line.split(';')
             else:
-                #separator is ','
+                # separator is ','
                 line = line.split(',')
             nomes.append(line[0])
             emails.append(line[1])
-        #dados.append(nome)
-        #dados.append(email)
+        # dados.append(nome)
+        # dados.append(email)
 
         emails_aux = []
-        #passando emails para minusculo
+        # passando emails para minusculo
         for email in emails:
             emails_aux.append(email.lower())
 
         return nomes, emails_aux
 
     def writeListPeople(self, people):
-        #A planilha é enviada para ua lista.txt em statics
+        # A planilha é enviada para ua lista.txt em statics
         aux_is_csv = str(people)
         if aux_is_csv[-4:] == '.csv':
             with open('statics/people_list.txt', 'wb+', ) as destination:
@@ -86,9 +86,9 @@ class FileFieldView(LoginRequiredMixin,FormView):
         return False
 
     def filterEmails(self, emails):
-        #Verifica se é um email válido
+        # Verifica se é um email válido
         r = re.compile(r'^[a-zA-Z0-9._-]+@(?:[a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}$')
-        #filtered_emails = [email for email in emails if r.match(email)]
+        # filtered_emails = [email for email in emails if r.match(email)]
         for email in emails:
             if not r.match(email):
                 return False
@@ -101,15 +101,15 @@ class FileFieldView(LoginRequiredMixin,FormView):
                 return False
         return True
 
-    def data_isValid(self, nomes,emails,certificados):
-        #Se o tamanho em caracteres for o mesmo, quer dizer que os arquivos são correspondentes
+    def data_isValid(self, nomes, emails, certificados):
+        # Se o tamanho em caracteres for o mesmo, quer dizer que os arquivos são correspondentes
         erro = ''
         # If the every registers have the same length
         if len(nomes) == len(emails) == len(certificados):
             result_0 = self.filterEmails(emails)
             result_1 = self.filterNames(nomes)
             if result_0 == False:
-                erro +='ERRO: Existe um ou mais registros na coluna de Emails incorretos'
+                erro += 'ERRO: Existe um ou mais registros na coluna de Emails incorretos'
                 return erro
             elif result_1 == False:
                 erro += 'ERRO: Existe um ou mais registros na coluna de Nomes invalidos'
@@ -119,7 +119,7 @@ class FileFieldView(LoginRequiredMixin,FormView):
                 return True
         else:
             erro += 'ERRO: Algum arquivo(lista das pessoas ou a relação de certificados)' \
-                   ' tem mais registros que o outro. Verifique se os arquivos são correspondente.\n'
+                    ' tem mais registros que o outro. Verifique se os arquivos são correspondente.\n'
             return erro
 
     def get(self, request, *args, **kwargs):
@@ -127,8 +127,7 @@ class FileFieldView(LoginRequiredMixin,FormView):
             # Se o usuário não for staff, ele não pode acessar essa view
             return redirect('certificado_url')
         else:
-            return render(request,'app_certificate/upload.html',{'form':self.form_class})
-
+            return render(request, 'app_certificate/upload.html', {'form': self.form_class})
 
     def post(self, request, *args, **kwargs):
 
@@ -158,67 +157,74 @@ class FileFieldView(LoginRequiredMixin,FormView):
                     try:
                         person = CustomUser.objects.get(email=e)
                     except CustomUser.DoesNotExist:
-                        #If there's not this person
+                        # If there's not this person
                         person = CustomUser.objects.create_user(email=e, password='321321', first_name=n, last_name='')
                         person.save()
 
-                    new_certificate = Certificate(certificateTitle=form.data['certificateTitle'],certificate=f,date=form.data['date'],user=person)
+                    new_certificate = Certificate(certificateTitle=form.data['certificateTitle'], certificate=f,
+                                                  date=form.data['date'], user=person)
                     new_certificate.save()
 
-                    #filetest = TesteModel(fileTest=f)
-                    #filetest.save()
+                    # filetest = TesteModel(fileTest=f)
+                    # filetest.save()
             else:
                 erros = [result]
                 print('here')
-                return render(request,'app_certificate/erros.html',{'erros': erros})
+                return render(request, 'app_certificate/erros.html', {'erros': erros})
             return self.form_valid(form)
         else:
             return self.form_invalid(form)
 
-class ShowCertificate(LoginRequiredMixin,View):
+
+class ShowCertificate(LoginRequiredMixin, View):
 
     def get(self, request, *args, **kwargs):
-
         date = Certificate.objects.filter(user__id=request.user.id).order_by('date')
         if request.user.is_staff:
             pass
-        return render(request,'app_certificate/show_certificate.html',{'date':date})
+        return render(request, 'app_certificate/show_certificate.html', {'date': date})
 
-class ListPeople(LoginRequiredMixin,ListView):
+
+class ListPeople(LoginRequiredMixin, ListView):
     model = CustomUser
     template_name = 'app_certificate/people_list.html'
 
-class PersonUpdate(LoginRequiredMixin,UpdateView):
+
+class PersonUpdate(LoginRequiredMixin, UpdateView):
     model = CustomUser
-    fields = ['first_name','last_name','email']
+    fields = ['first_name', 'last_name', 'email']
     template_name = 'app_certificate/person_update.html'
     success_url = reverse_lazy('pessoas_url')
 
     def form_valid(self, form):
-        """If the form is valid, redirect to the supplied URL."""
-        return self.render_to_response({'form':form, 'success':True})
+        messages.success(self.request, "Os dados foram atualizados com sucesso!")
+
+        super().form_valid(form)
+        return HttpResponseRedirect(self.get_success_url())
 
 
-class RemoveCertificate(LoginRequiredMixin,View):
-    #nada = Certificate.objects.filter()
+class RemoveCertificate(LoginRequiredMixin, View):
+    # nada = Certificate.objects.filter()
     outracoisa = Certificate._delete_file("uploads/certificates/museo3.jpg")
 
-class person_certificates(LoginRequiredMixin,View):
+
+class person_certificates(LoginRequiredMixin, View):
 
     def get(self, request, *args, **kwargs):
-
         certificates = Certificate.objects.filter(user__pk=kwargs['pk']).order_by('date')
 
+        return render(request, 'app_certificate/person_certificates.html', {'certificates': certificates})
 
-        return render(request,'app_certificate/person_certificates.html',{'certificates':certificates})
 
-class update_certificate(LoginRequiredMixin,UpdateView):
-    #TODO: Precisarei trocar mudar a forma que os dados são atualizados, isso porque o arquivo não é sobrescrito pelo novo, ficando o lixo armazenado.
+class update_certificate(LoginRequiredMixin, UpdateView):
+    # TODO: Precisarei trocar mudar a forma que os dados são atualizados, isso porque o arquivo não é sobrescrito pelo novo, ficando o lixo armazenado.
     model = Certificate
-    fields = ['certificate','date','certificateTitle']
+    fields = ['certificate', 'date', 'certificateTitle']
     template_name = 'app_certificate/update_certificate.html'
     success_url = reverse_lazy('pessoas_url')
 
     def form_valid(self, form):
-        """If the form is valid, redirect to the supplied URL."""
-        return self.render_to_response({'form':form, 'success':True})
+        messages.success(self.request, "Os dados foram atualizados com sucesso!")
+
+        super().form_valid(form)
+        return HttpResponseRedirect(self.get_success_url())
