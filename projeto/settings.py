18c80e78c0a449b5e7b0ab22365aa0501ea95701
Luiz Araujo <Luiz.araujodtna@gmail.com>
2021-01-05 20:15:14 -0300

    make it better

--- projeto/settings.py
+++ projeto/settings.py
@@ -24,7 +24,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = ['certificadosdead.herokuapp.com', 'localhost']
 
